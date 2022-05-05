7bf1f612327678865def50af43aae84281708a8b
Chatur Jodhani <jodhanichatur9@gmail.com>
2022-05-05 18:07:22 +0530

    first commit

---
+++ TrelloClone/TrelloCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrelloCloneApp.swift
+//  TrelloClone
+//
+//  Created by Chirag on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TrelloCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BoardView()
+        }
+    }
+}
